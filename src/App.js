b0ab649c2a396b836353cbde2078b5b88969f150
huqinlong <huqingl@hotmail.com>
2024-04-19 15:37:07 +0800

    finished branch three_nums_new

--- src/App.js
+++ src/App.js
@@ -11,16 +11,16 @@ function Square({ dataHasNum, children }) {
     </div>
   );
 }
-function PredictSquare({ dataHasNum,cols, children, makeNum }) {
-  const setNum = (i,col) => {
-    makeNum(i,col);
+function PredictSquare({ dataHasNum, cols, children, makeNum }) {
+  const setNum = (i, col) => {
+    makeNum(i, col);
   };
   return (
     <div
       className="square w-5 h-5 flex justify-center items-center cursor-pointer"
       data-has-num={dataHasNum}
       data-has-cols={cols}
-      onClick={() => setNum(dataHasNum,cols)}
+      onClick={() => setNum(dataHasNum, cols)}
     >
       {children}
     </div>
@@ -54,7 +54,7 @@ function Row({ num, bgColor }) {
     </div>
   );
 }
-function PredictRow({ num, bgColor, makeNum, cols ,currentCol}) {
+function PredictRow({ num, bgColor, makeNum, cols, currentCol }) {
   // useEffect(()=>{
   // console.log(one)
   // },[])
@@ -62,7 +62,12 @@ function PredictRow({ num, bgColor, makeNum, cols ,currentCol}) {
     <div className="row mr-2">
       <div className="flex">
         {Array.from(new Array(10), (v, i) => {
-          const el = i === num && currentCol ? <Number num={num} bgColor={bgColor} /> : "";
+          const el =
+            i === num && currentCol ? (
+              <Number num={num} bgColor={bgColor} />
+            ) : (
+              ""
+            );
           return (
             <PredictSquare
               key={i}
@@ -90,63 +95,61 @@ function App() {
   var oneRef = useRef();
   var allRef = useRef();
   // var prev = "000";
-  const colors = ['#991b1b', '#0284c7', '#166534', '#a16207', '#155e75']
+  const colors = ["#991b1b", "#0284c7", "#166534", "#a16207", "#155e75"];
   useEffect(() => {
-    axios
-      .get("https://huqinlong.com/d/houtai/get_history.php")
-      .then((res) => {
-        let prev = res.data[0];
-        let one = [],
-          two = [],
-          three = [],
-          sumEnd = [],
-          sumDiff = [];
-        let tempData = res.data.slice(1);
-        tempData.map((item, index) => {
-          one.push(parseInt(item.charAt(0)));
-          oneRef.current = one;
-          two.push(parseInt(item.charAt(1)));
-          three.push(parseInt(item.charAt(2)));
-          // setOne([...one, parseInt(item.charAt(0))]);
-          // setTwo([...two, parseInt(item.charAt(0))]);
-          // setThree([...three, parseInt(item.charAt(0))]);
-          let sum =
-            parseInt(item.charAt(0)) +
-            parseInt(item.charAt(1)) +
-            parseInt(item.charAt(2));
-          let sumE = parseInt(sum % 10);
-          sumEnd.push(sumE);
-          // setSumEnd([...sumEnd, sumE]);
-          if (index === 0) {
-            var sumD = Math.abs(
-              sumE -
+    axios.get("https://huqinlong.com/d/houtai/get_history.php").then((res) => {
+      let prev = res.data[0];
+      let one = [],
+        two = [],
+        three = [],
+        sumEnd = [],
+        sumDiff = [];
+      let tempData = res.data.slice(1);
+      tempData.forEach((item, index) => {
+        one.push(parseInt(item.charAt(0)));
+        oneRef.current = one;
+        two.push(parseInt(item.charAt(1)));
+        three.push(parseInt(item.charAt(2)));
+        // setOne([...one, parseInt(item.charAt(0))]);
+        // setTwo([...two, parseInt(item.charAt(0))]);
+        // setThree([...three, parseInt(item.charAt(0))]);
+        let sum =
+          parseInt(item.charAt(0)) +
+          parseInt(item.charAt(1)) +
+          parseInt(item.charAt(2));
+        let sumE = parseInt(sum % 10);
+        sumEnd.push(sumE);
+        // setSumEnd([...sumEnd, sumE]);
+        if (index === 0) {
+          var sumD = Math.abs(
+            sumE -
               parseInt(
                 (parseInt(prev.charAt(0)) +
                   parseInt(prev.charAt(1)) +
                   parseInt(prev.charAt(2))) %
-                10
+                  10
               )
-            );
-          } else {
-            sumD = Math.abs(sumEnd[index] - sumEnd[index - 1]);
-          }
-          sumDiff.push(sumD);
-          // setSumDiff([...sumDiff, sumDiff]);
-        });
-        // all.push(one, two, three, sumEnd, sumDiff);
-        // console.log(all);
-        allRef.current = [one, two, three, sumEnd, sumDiff]
-        // setOne(one);
-        // oneRef.current = one;
-        // setTwo(two);
-        // setThree(three);
-        // setSumEnd(sumEnd);
-        // setSumDiff(sumDiff);
-        // if (!ignore) {
-        setAllData([one, two, three, sumEnd, sumDiff]);
-        // }
-        // console.log(allData);
+          );
+        } else {
+          sumD = Math.abs(sumEnd[index] - sumEnd[index - 1]);
+        }
+        sumDiff.push(sumD);
+        // setSumDiff([...sumDiff, sumDiff]);
       });
+      // all.push(one, two, three, sumEnd, sumDiff);
+      // console.log(all);
+      allRef.current = [one, two, three, sumEnd, sumDiff];
+      // setOne(one);
+      // oneRef.current = one;
+      // setTwo(two);
+      // setThree(three);
+      // setSumEnd(sumEnd);
+      // setSumDiff(sumDiff);
+      // if (!ignore) {
+      setAllData([one, two, three, sumEnd, sumDiff]);
+      // }
+      // console.log(allData);
+    });
     // return () => {
     //   ignore = true;
     // }
@@ -157,7 +160,7 @@ function App() {
 
   const Draw = (data, id) => {
     // console.log(data)
-    let cnv = document.getElementById('canvas' + id);
+    let cnv = document.getElementById("canvas" + id);
     let cxt = cnv.getContext("2d");
     cxt.beginPath();
 
@@ -165,7 +168,7 @@ function App() {
     let startX = startNum * 20 + 10;
     let startY = 10;
     cxt.moveTo(startX, startY);
-    data.map((item, index) => {
+    data.forEach((item, index) => {
       if (index > 0) {
         let x = item * 20 + 10;
         let y = index * 20 + 10;
@@ -176,64 +179,76 @@ function App() {
   };
   useEffect(() => {
     setTimeout(() => {
-      const oneLenth = oneRef.current.length;
-      const oneWidth = 20 * 10;
-      const oneHeight = 20 * oneLenth;
-      setOneWidth(oneWidth);
-      setOneHeight(oneHeight);
+      const onelenth = oneRef.current.length;
+      const onewidth = 20 * 10;
+      const oneheight = 20 * onelenth;
+      setOneWidth(onewidth);
+      setOneHeight(oneheight);
       allRef.current.forEach((item, k) => {
-        Draw(item, k)
+        Draw(item, k);
       });
-      // Draw(one, "canvas");
     }, 2000);
   });
-  // const predictDraw = (predictNum, data, id, No) => {
-  //   //设置连接线所在canvas画布的宽度
-  //   let lastNum = data[data.length - 1];
-  //   let width = Math.abs(lastNum - predictNum) * 20;
-  //   let top = oneHeight - 10;
-  //   var left;
-  //   if (lastNum < predictNum) {
-  //     left = lastNum * 20 + 10;
-  //   } else {
-  //     left = predictNum * 20 + 10;
-  //   }
-  //   switch (No) {
-  //     case "":
-  //       setPredictWidth(width);
-  //       setPredictTop(top);
-  //       setPredictLeft(left);
-  //       break;
-  //   }
-  //   //设置画布position
+  const predictDraw = (predictNum, data, No) => {
+    //设置连接线所在canvas画布的宽度
+    let lastNum = data[data.length - 1];
+    let width = Math.abs(lastNum - predictNum) * 20;
+    // let top = oneHeight - 10;
+    var left;
+    if (lastNum < predictNum) {
+      left = lastNum * 20 + 10;
+    } else {
+      left = predictNum * 20 + 10;
+    }
+    let a = predictLeft.map((item, k) => {
+      if(k === parseInt(No)){
+        return left
+      }
+      return item;
+    });
+    setPredictLeft(a)
+    let b = predictWidth.map((item, k) => {
+      if(k === parseInt(No)){
+        return width
+      }
+      return item;
+    });
+    setPredictWidth(b)
+  
+    //开始画线
+    let cnv = document.getElementById('predict-canvas'+No);
+    let cxt = cnv.getContext("2d");
+    cxt.clearRect(0, 0, 200, 20);
+    setTimeout(() => {
+      cxt.beginPath();
+      if (lastNum < predictNum) {
+        cxt.moveTo(0, 0);
+        cxt.lineTo(width, 20);
+      } else {
+        cxt.moveTo(width, 0);
+        cxt.lineTo(0, 20);
+      }
+      cxt.stroke();
+    }, 500);
+  };
 
-  //   //开始画线
-  //   let cnv = document.getElementById(id);
-  //   let cxt = cnv.getContext("2d");
-  //   cxt.clearRect(0, 0, 200, 20);
-  //   setTimeout(() => {
-  //     cxt.beginPath();
-  //     if (lastNum < predictNum) {
-  //       cxt.moveTo(0, 0);
-  //       cxt.lineTo(width, 20);
-  //     } else {
-  //       cxt.moveTo(width, 0);
-  //       cxt.lineTo(0, 20);
-  //     }
-  //     cxt.stroke();
-  //   }, 500);
-  // };
-  const [num, setNum] = useState();
-  const [col, setCol] = useState();
-  const [predictWidth, setPredictWidth] = useState(0);
-  const [predictTop, setPredictTop] = useState(0);
-  const [predictLeft, setPredictLeft] = useState(0);
 
-  const makeNum = (predictNum,col) => {
-    //设置点击选择要显示的数字
-    setNum(predictNum);
-    setCol(col)
-    // predictDraw(predictNum, one, "predict-canvas", "");
+  const [nums, setNums] = useState(['','','','','']);
+  const [col, setCol] = useState([]);
+
+  const [predictWidth, setPredictWidth] = useState(['','','','','']);
+  const [predictLeft, setPredictLeft] = useState(['','','','','']);
+
+  const makeNum = (predictNum, c) => {
+    let a = nums.map((item, k) => {
+      if(k === parseInt(c)){
+        return predictNum
+      }
+      return item;
+    });
+    setNums(a);
+    setCol([...col, c]);
+    predictDraw(predictNum,allRef.current[c], c)
   };
 
   return (
@@ -245,21 +260,27 @@ function App() {
               {i.map((item, index) => {
                 return <Row num={item} bgColor={colors[k]} key={index} />;
               })}
-              <PredictRow num={num} bgColor={colors[k]} makeNum={makeNum} cols={k} currentCol = {col === k}/>
-              {/* <canvas
+              <PredictRow
+                num={nums[k]}
+                bgColor={colors[k]}
+                makeNum={makeNum}
+                cols={k}
+                currentCol={col.indexOf(k) !== -1}
+              />
+              <canvas
                 // id="predict-canvas"
                 id={`predict-canvas${k}`}
-                width={predictWidth}
+                width={predictWidth[k]}
                 height={20}
                 color="#111"
                 style={{
                   position: "absolute",
-                  top: predictTop,
-                  left: predictLeft,
+                  top: i.length * 20 - 10,
+                  left: predictLeft[k],
                   zIndex: 2,
                   // border: "1px dashed #111",
                 }}
-              ></canvas> */}
+              ></canvas>
               <canvas
                 id={`canvas${k}`}
                 width={oneWidth}
