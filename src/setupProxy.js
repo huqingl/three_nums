76e09756ada2dc62c0d5b2a9cc5681ef8b0d2971
huqinlong <huqingl@hotmail.com>
2024-08-10 18:40:00 +0800

    change domain

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
     createProxyMiddleware("/d", {
-      target: "https://huqinlong.com/d/",
+      target: "http://yuduntech.com:8070/d/",
       // target: "http://shunyuanchat.site",
       changeOrigin: false,
       secure: false,
